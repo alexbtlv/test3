f575c99001ed96a9dfcf2dcc5d3104229a2744df
Alexander <alexbtlv@gmail.com>
2019-04-12 17:30:49 +0800

    Refactored create transaction VC

--- test3/View/CreateTransaction/CreateTransactionViewController.swift
+++ test3/View/CreateTransaction/CreateTransactionViewController.swift
@@ -11,16 +11,17 @@ import MBProgressHUD
 
 class CreateTransactionViewController: UIViewController {
     
-    @IBOutlet private weak var recipientTextField: BindingSearchTextField!
+    @IBOutlet weak var recipientTextField: BindingSearchTextField!
     @IBOutlet private weak var amountTextField: BindingTextField!
     
-    private let newTransaction = PotentialTransactionViewModel()
+    private var newTransaction: PotentialTransactionViewModel!
 
     var userVM: UserViewModel!
     var transactionCopy: TransactionViewModel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        newTransaction = PotentialTransactionViewModel(view: self)
         setupUI()
     }
 
@@ -37,9 +38,7 @@ class CreateTransactionViewController: UIViewController {
         
         recipientTextField.bind { [unowned self] in
             self.newTransaction.recipient.value = $0
-            if !$0.isEmpty {
-                self.getUsernamesForAutocomplete(query: $0)
-            }
+            self.newTransaction.getUsernamesForAutocomplete()
         }
         
         recipientTextField.itemSelectionHandler = {  [unowned self] filteredResults, itemPosition in
@@ -56,44 +55,9 @@ class CreateTransactionViewController: UIViewController {
     
     @IBAction func sendButtonTapped(_ sender: Any) {
         if newTransaction.isValid {
-            MBProgressHUD.showAdded(to: view, animated: true)
-            
-            let transactionFuture = NetworkingManager.sendTransaction(recipient: newTransaction.recipient.value!, amount: newTransaction.amount.value!)
-            transactionFuture.execute { [weak self] result in
-                guard let self = self else { return }
-                MBProgressHUD.hide(for: self.view, animated: true)
-                switch result {
-                case .success(let transactionToken):
-                    if let accountVC = self.previousViewController as? AccountViewController {
-                        accountVC.fetchData()
-                    }
-                    let transactionVM = TransactionViewModel(transactionToken.transaction)!
-                    let alertVC = UIAlertController(title: "Success", message: "Transaction succesful!\n \(self.userVM.name) -->\(transactionVM.amountText) \(transactionVM.recipient)", preferredStyle: .alert)
-                    let okAction = UIAlertAction(title: "Ok", style: .cancel, handler: {_ in
-                        self.navigationController?.popViewController(animated: true)
-                    })
-                    alertVC.addAction(okAction)
-                    self.present(alertVC, animated: true, completion: nil)
-                case .failure(let error):
-                    self.showAlert(withMessage: error.localizedDescription)
-                }
-            }
+            newTransaction.sendTransaction()
         } else {
             showAlert(withMessage: newTransaction.validationMessage)
-        }
-    }
-    
-    private func getUsernamesForAutocomplete(query: String) {
-        let namesFuture = NetworkingManager.getUsernames(query: query)
-        namesFuture.execute { [weak self] result in
-            guard let self = self else { return }
-            switch result {
-            case .success(let recipients):
-                let names = recipients.map { $0.name }
-                self.recipientTextField.filterStrings(names)
-            case .failure(let errorMessage):
-                self.showAlert(withMessage: errorMessage.localizedDescription)
-            }
         }
     }
 }
--- test3/ViewModel/PotentialTransactionViewModel.swift
+++ test3/ViewModel/PotentialTransactionViewModel.swift
@@ -6,9 +6,12 @@
 //  Copyright © 2019 Alexander Batalov. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import MBProgressHUD
 
 class PotentialTransactionViewModel: Validatable {
+    
+    internal unowned let view: CreateTransactionViewController
     
     var recipient = Dynamic<String>("")
     var amount = Dynamic<Int>(0)
@@ -28,17 +31,64 @@ class PotentialTransactionViewModel: Validatable {
         return message
     }
     
+    init(view: CreateTransactionViewController) {
+        self.view = view
+    }
+    
     private func validate() {
         if let name = recipient.value, name.isEmpty {
             brokenRules.append(BrokenRule(propertyName: "name", message: "Name can not be empty"))
         }
         
-        if let amount = amount.value, amount > senderBalance {
+        if let amount = amount.value, amount > senderBalance || amount < 0 {
             brokenRules.append(BrokenRule(propertyName: "amount", message: "Insufficient funds. Please top up your balance."))
         }
         
         if let amount = amount.value, amount == 0 {
             brokenRules.append(BrokenRule(propertyName: "amount", message: "Please enter valid amount."))
+        }
+    }
+    
+    func getUsernamesForAutocomplete() {
+        guard let query = recipient.value else { return }
+        let namesFuture = NetworkingManager.getUsernames(query: query)
+        namesFuture.execute { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let recipients):
+                let names = recipients.map { $0.name }
+                self.view.recipientTextField.filterStrings(names)
+            case .failure(let errorMessage):
+                self.view.showAlert(withMessage: errorMessage.localizedDescription)
+            }
+        }
+    }
+    
+    func sendTransaction() {
+        guard let amount = amount.value, let recipient = recipient.value else {
+            preconditionFailure("Make sure to valide inputs")
+        }
+        MBProgressHUD.showAdded(to: view.view, animated: true)
+        
+        let transactionFuture = NetworkingManager.sendTransaction(recipient: recipient, amount: amount)
+        transactionFuture.execute { [weak self] result in
+            guard let self = self else { return }
+            MBProgressHUD.hide(for: self.view.view, animated: true)
+            switch result {
+            case .success(let transactionToken):
+                if let accountVC = self.view.previousViewController as? AccountViewController {
+                    accountVC.fetchData()
+                }
+                let transactionVM = TransactionViewModel(transactionToken.transaction)!
+                let alertVC = UIAlertController(title: "Success", message: "Transaction succesful!\n \(self.view.userVM.name) -->\(transactionVM.amountText) \(transactionVM.recipient)", preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "Ok", style: .cancel, handler: {_ in
+                    self.view.navigationController?.popViewController(animated: true)
+                })
+                alertVC.addAction(okAction)
+                self.view.present(alertVC, animated: true, completion: nil)
+            case .failure(let error):
+                self.view.showAlert(withMessage: error.localizedDescription)
+            }
         }
     }
 }
